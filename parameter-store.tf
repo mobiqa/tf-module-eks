5773adfda898e7bd1dfe7dc14923ea1322dc828e
venkat <venkat.mobiqa@gmail.com>
2023-07-05 10:46:53 +0530

    updating ./tf-module-eks

--- parameter-store.tf
+++ parameter-store.tf
@@ -26,31 +26,31 @@ resource "aws_iam_policy" "parameter-store-serviceaccount-policy" {
 }
 
 
-#data "aws_iam_policy_document" "parameter-store-policy_document" {
-#  statement {
-#    actions = ["sts:AssumeRoleWithWebIdentity"]
-#
-#    condition {
-#      test = "StringEquals"
-#      variable = "${replace(
-#        aws_eks_cluster.eks.identity[0].oidc[0].issuer,
-#        "https://",
-#        "",
-#      )}:aud"
-#      values = ["sts.amazonaws.com"]
-#    }
-#
-#    principals {
-#      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/${replace(
-#        aws_eks_cluster.eks.identity[0].oidc[0].issuer,
-#        "https://",
-#        "",
-#      )}"]
-#      type = "Federated"
-#    }
-#  }
-#}
-#
+data "aws_iam_policy_document" "parameter-store-policy_document" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    condition {
+      test = "StringEquals"
+      variable = "${replace(
+        aws_eks_cluster.eks.identity[0].oidc[0].issuer,
+        "https://",
+        "",
+      )}:aud"
+      values = ["sts.amazonaws.com"]
+    }
+
+    principals {
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/${replace(
+        aws_eks_cluster.eks.identity[0].oidc[0].issuer,
+        "https://",
+        "",
+      )}"]
+      type = "Federated"
+    }
+  }
+}
+
 #resource "aws_iam_role" "parameter-store-oidc-role" {
 #  count       = var.CREATE_PARAMETER_STORE ? 1 : 0
 #  name               = "parameter-store-role-with-oidc"
