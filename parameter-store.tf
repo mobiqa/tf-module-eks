9199396071872d732eb61009fc99fe1c2e55acf2
venkat <venkat.mobiqa@gmail.com>
2023-07-05 11:19:02 +0530

    updating ./tf-module-eks

--- parameter-store.tf
+++ parameter-store.tf
@@ -51,17 +51,17 @@ data "aws_iam_policy_document" "parameter-store-policy_document" {
   }
 }
 
-#resource "aws_iam_role" "parameter-store-oidc-role" {
-#  count       = var.CREATE_PARAMETER_STORE ? 1 : 0
-#  name               = "parameter-store-role-with-oidc"
-#  assume_role_policy = data.aws_iam_policy_document.external-secrets-policy_document.json
-#}
-#
-#resource "aws_iam_role_policy_attachment" "parameter-store-role-attach" {
-#  count       = var.CREATE_PARAMETER_STORE ? 1 : 0
-#  role       = aws_iam_role.parameter-store-oidc-role.*.name[0]
-#  policy_arn = aws_iam_policy.parameter-store-serviceaccount-policy.*.arn[0]
-#}
-#
-#
-#
+resource "aws_iam_role" "parameter-store-oidc-role" {
+  count       = var.CREATE_PARAMETER_STORE ? 1 : 0
+  name               = "parameter-store-role-with-oidc"
+  assume_role_policy = data.aws_iam_policy_document.external-secrets-policy_document.json
+}
+
+resource "aws_iam_role_policy_attachment" "parameter-store-role-attach" {
+  count       = var.CREATE_PARAMETER_STORE ? 1 : 0
+  role       = aws_iam_role.parameter-store-oidc-role.*.name[0]
+  policy_arn = aws_iam_policy.parameter-store-serviceaccount-policy.*.arn[0]
+}
+
+
+
